b6c7a0d4988d3270d8a78740da2d38b9a6e632a4
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2017-07-25 21:45:33 -0500

    Get data from github and gitlab. Show a timeline. TODO: Fix issues with dates

--- scripts/app.js
+++ scripts/app.js
@@ -12,68 +12,113 @@
       });
     });
 
-    var github = d3.select('#github-viz');
-    console.log(github);
-    $.get('https://api.github.com/users/blankazucenalg/events', function(data) {
-      console.log(data);
-      data.forEach(function(d) {
-        d.created_at = new Date(d.created_at);
-      });
-      var nestedData = d3.nest()
-        .key(function(d){ return d.created_at.getFullYear() + '-' + d.created_at.getMonth() + '-' + d.created_at.getDate(); })
-        .entries(data);
+    var container = d3.select('#github-viz');
+    console.log(container);
+    $.get('https://api.github.com/users/blankazucenalg/events', function(github) {
+      $.ajax({
+        type: "GET",
+        beforeSend: function(request) {
+          request.setRequestHeader("PRIVATE-TOKEN", '5TNYhcMkdkBn7RAh3qPK');
+        },
+        url: "https://gitlab.com/api/v4/users/blankazucenalg/events",
+        success: function(gitlab) {
+        var data = [];
+        for (var i = github.length - 1; i >= 0; i--) {
+          data.push(github[i]);
+        }
+        for (var i = gitlab.length - 1; i >= 0; i--) {
+          console.log(gitlab[i])
+          data.push(gitlab[i]);
+        }
+        console.log(data);
+        data.forEach(function(d) {
+          d.created_at = moment(d.created_at);
+        });
+        data.sort(function(a,b) {
+          return b.created_at - a.created_at;
+        })
 
-      console.log(nestedData);
+        var nestedData = d3.nest()
+          .key(function(d){ return moment(d.created_at).format('Y/MM/D') })
+          .entries(data);
 
-      var today = new Date();
-      var first = data[data.length - 1].created_at;
-      var daysDiff = Math.round((today-first)/(1000*60*60*24));
-      var size = 30;
+        console.log(nestedData);
 
-      var width = $(github.node()).width();
-      var height = Math.ceil(daysDiff / Math.floor(width / size));
-      var svg = github.append('svg')
-        .attr('width', width)
-        .attr('height', height);
-      console.log(width, height);
+        var today = moment();
+        var first = moment(data[data.length - 1].created_at);
 
-      var tip = d3.tip().attr('class','d3-tip').html(function(d){
-        console.log(d);
-        return '<p>' + d.type + '</p><p>' + d.created_at + '</p>';
-      });
-      svg.call(tip);
+        d3.select('#first-date').text(moment(first).format('YYYY/MM/DD'));
+        d3.select('#today-date').text(moment(today).format('YYYY/MM/DD'));
 
-      
-      var scaleX = d3.scaleLinear()
-        .range([0, width])
-        .domain([0, Math.floor(width / size)])
+        var daysDiff = Math.round((today-first)/(1000*60*60*24));
+        var width = $(container.node()).width();
+        var height = 30;
+        var size = width / daysDiff;
 
-      var scaleY = d3.scaleLinear()
-        .range([0, height])
-        .domain([0, Math.ceil(height / size)])
-      // Math.ceil(data.length / Math.floor(width / size))
+        var scale = d3.scaleLinear()
+          .range([0, width - size])
+          .domain([first, today]);
 
-      
+        var svg = container.append('svg')
+          .attr('width', width)
+          .attr('height', height);
+        console.log(width, height);
 
-      for (var i = 0; i < daysDiff; i++) {
+        var tip = d3.tip().attr('class','d3-tip').html(function(d){
+          console.log(d);
+          var html = '<span>' + d.values.length + ' events</span><br>'
+          d.values.forEach(function(o){
+            html += ''; //<span>' + (o.type || o.action_name) + '</span><br>'
+          })
+          html += '<span>' + moment(d.created_at).format('DD/MM/YYYY') + '</span>';
+          return html;
+        });
+
+        svg.call(tip);
+
         svg.append('rect')
           .attr('fill', '#eee')
-          .attr('x', function(d,i) { return scaleX(i); })
-          .attr('y', function(d,i) { return scaleY(Math.floor(i / Math.floor(width / size))); } )
-          .attr('width', size)
-          .attr('height', size)
-      }
-      svg.selectAll('.event')
-        .data(data)
-        .enter()
-        .append('rect')
-          .attr('class', 'event')
-          .attr('x', function(d,i) { return scaleX(i); })
-          .attr('y', function(d,i) { return scaleY(Math.floor(i / Math.floor(width / size))); } )
-          .attr('width', size)
-          .attr('height', size)
-          .on('mouseover', tip.show)
-          .on('mouseout', tip.hide)
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('width', width)
+          .attr('height', size);
+
+        var nested = d3.nest()
+          .key(function(d) { return moment.tz(d.created_at, 'America/Mexico_City').format('YYYY/MM/DD'); })
+          .entries(data);
+
+        var eventsQuantity = d3.extent(nested, function(leave) { return leave.values.length; });
+        var opacity = d3.scaleLinear()
+          .range([0.3,1])
+          .domain(eventsQuantity);
+
+        console.log(nested);
+        
+        svg.selectAll('.event')
+          .data(nested)
+          .enter()
+          .append('rect')
+            .attr('class', 'event')
+            .attr('x', function(d,i) { return scale(moment(d.key)); })
+            .attr('y', 0)
+            .attr('width', size)
+            .attr('height', height)
+            .attr('opacity', function(d){ return opacity(d.values.length); })
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+            .on('click', function(d) {
+              var selection = d3.select('#content-code').selectAll('.eventDay')
+                .data(d.values, function(o) { return o.id; })
+              
+              selection.exit().remove();
+              selection.enter()
+                .append('p')
+                .attr('class', 'eventDay')
+                .text(function(o){ return JSON.stringify(o); })
+
+            })
+        }
+      });
     });
   });
 })();
